bfba5248a238ec8425ff2f8cd082993fd0eca89d
root <root@localhost>
2025-06-24 15:12:59 +0000

    test

---
+++ lib/gcc/aarch64-linux-android/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.4/configure --target=aarch64-linux-android --prefix=/opt/aarch64-linux-android-4.9 --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
